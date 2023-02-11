cef6b6d6732ec54d464d7bb4a0f022a0d76e4e20
Wilmer Rivera <wilmerbaby@gmail.com>
2023-02-11 11:43:54 -0500

    testing .exe file

--- controllers/front.go
+++ controllers/front.go
@@ -6,22 +6,23 @@ when a  network request recived
 	will direct to the correct controller to be processed
 */
 package controllers
+
 import (
 	"encoding/json"
 	"io"
 	"net/http"
 )
+
 // RegisterController responsibility is to create a newUserController
 func RegisterControllers() {
-
 	// newuserController is a constructor function
 	uc := newUserController()
-
 	// handeles http.handle with users without extra information
 	http.Handle("/users", *uc)
 	// handles http.handle with users with extra info
 	http.Handle("/users/", *uc)
 }
+
 /*
 	Reaches into encoding/json package, creates an encoder that is designed to encode Go objects into JSON representations,
 
